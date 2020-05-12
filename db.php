fd4526b2feec03ec0652d11fe4bf35aa6ea2b921
cyd <michellechen58@gmail.com>
2020-05-12 15:24:53 +0800

    utf8編碼

--- db.php
+++ db.php
@@ -1,15 +1,17 @@
 <?php
   $host = "localhost";
   $dbname = "sa";
-  $username = "SA";
+  $username = "root";
   $password = "";
   
   try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->query('SET NAMES "utf8"');  
   
   }
   catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
   }
+
 ?>
