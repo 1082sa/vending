4da50ee5f26a997865124ad98c58431713e6b9fc
shinyu0430 <0430shinyu@gmail.com>
2020-05-17 00:47:10 +0800

    刪掉沒用到的地方

--- ven-info.php
+++ ven-info.php
@@ -47,39 +47,7 @@
                          
                        <?php }?>
                 
-                <div class="ven_item">
-                    <div class="ven_item_img">
-                        <img src="img/blog-img/3.jpg">
-                    </div>
-                    <div class="ven_item_name text-center">
-                        <p>商品一</p>
-                    </div>
-                    <div class="ven_item_info text-center">
-                        <p><span class="ven_item_price">QQ</span>元，剩餘<span class="ven_item_quantity">XX</span>個</p>
-                    </div>
-                </div>
-                <div class="ven_item">
-                    <div class="ven_item_img">
-                        <img src="img/blog-img/4.jpg">
-                    </div>
-                    <div class="ven_item_name text-center">
-                        <p>商品二</p>
-                    </div>
-                    <div class="ven_item_info text-center">
-                        <p><span class="ven_item_price">QQ</span>元，剩餘<span class="ven_item_quantity">XX</span>個</p>
-                    </div>
-                </div>
-                <div class="ven_item">
-                    <div class="ven_item_img">
-                        <img src="img/blog-img/5.jpg">
-                    </div>
-                    <div class="ven_item_name text-center">
-                        <p>商品三</p>
-                    </div>
-                    <div class="ven_item_info text-center">
-                        <p><span class="ven_item_price">QQ</span>元，剩餘<span class="ven_item_quantity">XX</span>個</p>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
