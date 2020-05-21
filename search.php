4eace4af0b2387c9e9263231c5a7ef26ef690e61
shinyu0430 <0430shinyu@gmail.com>
2020-05-21 17:31:26 +0800

    搜尋有bug改一點點而已

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ $keyword = $_POST["searching_keyword"];
 
 
   $statement = $conn->query("SELECT distinct sort.pro_name ,picture.pro_pic  FROM `sort`,`picture` ,`information`
-WHERE sort.pro_name or sort.sort_name LIKE'%$keyword%'
+WHERE (sort.pro_name LIKE'%$keyword%' or sort.sort_name LIKE'%$keyword%')
 and sort.pro_name= information.pro_name AND
 sort.pro_name=picture.pro_name");
 
