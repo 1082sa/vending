bf9d8be141338a479f5dfdf8af288d92d465d461
shinyu0430 <0430shinyu@gmail.com>
2020-05-21 17:11:31 +0800

    搜尋功能（成功）

--- search.php
+++ search.php
@@ -1,10 +1,10 @@
 <? 
 require 'db.php';
-//$keyword = $_GET["searching_keyword"];
+$keyword = $_POST["searching_keyword"];
 
 
   $statement = $conn->query("SELECT distinct sort.pro_name ,picture.pro_pic  FROM `sort`,`picture` ,`information`
-WHERE sort.pro_name or sort.sort_name LIKE'綠茶'
+WHERE sort.pro_name or sort.sort_name LIKE'%$keyword%'
 and sort.pro_name= information.pro_name AND
 sort.pro_name=picture.pro_name");
 
