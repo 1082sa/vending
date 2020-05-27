b3d1e0ed4898906dca77e29bdf7f08ae3298d823
Andy <tty09183@gmail.com>
2020-05-27 20:39:13 +0800

    index.html修改完成，但index.php無解

--- index.php
+++ index.php
@@ -80,8 +80,14 @@
             <span aria-hidden="true">&times;</span>
           </button>
                 </div>
-
+                <div class="modal-body">
                     <?php include("ven-info.php");?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                    
                 
                 
             </div>
@@ -94,14 +100,12 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLongTitle">販賣機故障回報</h5>
-                     
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
                 </div>
                 <div class="modal-body">
-                    <?php
- include("contact.php");?>
+                    <?php include("contact.php");?>
                 </div>
                 
             </div>
