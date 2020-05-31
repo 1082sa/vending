1e4bb215a21c255e79c651e911821e808c9cfe3d
YuChingSu <stitch980827@gmail.com>
2020-05-31 19:07:49 +0800

    sidebar.php / 新增沒有搜尋功能的導覽列

---
+++ sidebar.php
@@ -0,0 +1,86 @@
+
+        <!-- Nav Area -->
+        <div class="original-nav-area" id="stickyNav">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Classy Menu -->
+                    <nav class="classy-navbar">
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span
+                ></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu" id="originalNav">
+                            <!-- close btn -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap">
+                                    <span class="top"></span><span class="bottom"></span>
+                                </div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <!-- Sidebar information -->
+                                <div class="sideinfo">
+                                    <div class="circle ml-15 mb-7">
+                                        <a href="profile.php"><img src="img/vending-machine.png" /></a>
+                                    </div>
+                                    &emsp;
+                                    <h4>Mavis</h4>
+                                </div>
+
+                                <ul>
+                                    <li>
+                                        <a href="index.php"><span>首頁</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><span>我的最愛</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><span>消費紀錄</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="contact.php"><span>故障回報紀錄</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><span>客服聯繫</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><span>活動優惠</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><span>行動支付</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="logout.php"><span>登出</span></a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>    
+
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+    <!--include html-->
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
+    <script>
+        w3.includeHTML();
+    </script>
+</body>
+
+</html>
