1af251458cc47f89f202b9393f35a79e1c5b73f5
yuyu <m22104416@gmail.com>
2020-06-01 22:01:08 +0800

    原本的login.html改成login.php

--- login.php
+++ login.php
@@ -1,28 +1,101 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
 
-require 'db.php';
-$account = $_POST["account"];
-$password = $_POST["password"];
+<head>
+    <meta charset="UTF-8" />
+    <meta name="description" content="" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-if( !isset($_POST['account']) || !isset($_POST['password']) || $_POST['account']=="" || $_POST['password']=="" ){
-	header("location:index.php");
-	}
-else{
-$sql = "SELECT * FROM `user` WHERE( `account` = ? AND `password` = ? )";
-$sth = $conn->prepare($sql);
-$sth->execute(array($account, $password));
-$result = $sth->fetch(PDO::FETCH_OBJ);
-    
-if($result) {
-	$_SESSION['LoginSuccess'] = true;
-    $_SESSION['account'] = $account;
-header("location:index.php"); 
-}
-else {
-	header("Location: login.php?msg=error");
-}
+    <!-- Title -->
+    <title>WEVEN - your vending machine</title>
 
-}
-?>
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico" />
 
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <link rel="manifest" href="manifest.json" />
+
+    <script>
+        if ("serviceWorker.js" in navigator) {
+            console.log("Will service worker register?");
+            navigator.serviceWorker
+                .register("service-worker.js")
+                .then(function(reg) {
+                    console.log("yes it did.");
+                })
+                .catch(function(err) {
+                    console.log("No it didn,t This happened: ", err);
+                });
+        }
+    </script>
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="preload-content">
+            <div id="original-load"></div>
+        </div>
+    </div>
+
+    <!-- ##### Blog Wrapper Start ##### -->
+    <div class="blog-wrapper section-padding-100 clearfix" style="text-align: center;">
+        <div class="container">
+            <div>
+                <!-- Single Blog Area -->
+                <div class="single-blog-area clearfix mb-100">
+                    <!-- Blog Content -->
+                    <div class="single-blog-content">
+                        <h4 style="font-weight: bold; text-align: center;">登入</h4><br />
+                        
+                         <hr size="10px" align="center" width="100%">
+						 <form action="login_process.php" method="post">
+                        <p style="padding-top:40px">
+                            帳號&emsp;&emsp;&nbsp;&emsp;<input type="email" name="account" size="18" class="text" placeholder="email" /><br /><br /> 密碼&emsp;&emsp;&nbsp;&emsp;
+                            <input type="password" name="password" size="18" class="text" placeholder="password" /><?php $msg = isset($_GET["msg"])? $_GET["msg"]: "";echo $msg; ?><br /><br />
+                            <a href="registration.html" class="post-tag">尚未註冊?</a>
+                            <br>
+                            <form action="index.html">
+                                <button type="submit" class="btn original-btn">登入</button>
+                            </form>
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ##### Footer Area Start ##### -->
+    <footer class="footer-area text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+
+
+
+                </div>
+            </div>
+        </div>
+
+
+    </footer>
+    <!-- ##### Footer Area End ##### -->
+
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Plugins js -->
+    <script src="js/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
