4707d5cc41402b7f0a3cf023bb309cc40ab7aa79
YuChingSu <stitch980827@gmail.com>
2020-06-06 01:01:27 +0800

    Merge branch 'master' of https://github.com/1082sa/vending

--- login.php
+++ login.php
@@ -19,6 +19,7 @@
 
     <link rel="manifest" href="manifest.json" />
 
+    <!--animation.css-->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"/>
 
     <script>
