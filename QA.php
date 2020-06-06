aadff6383388028da60db510a08ee85a3e8baedf
Andy <tty09183@gmail.com>
2020-06-06 22:12:09 +0800

    改變全部title

--- QA.php
+++ QA.php
@@ -9,7 +9,7 @@
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title -->
-    <title>WEVEN - your vending machine</title>
+    <title>WEVEN - 常見問題</title>
 
     <!-- Favicon -->
     <link rel="icon" href="img/core-img/favicon.ico" />
--- cs.php
+++ cs.php
@@ -9,7 +9,7 @@
     <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title -->
-    <title>WEVEN - your vending machine</title>
+    <title>WEVEN - 客服聯繫</title>
 
     <!-- Favicon -->
     <link rel="icon" href="img/core-img/favicon.ico" />
