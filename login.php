a620a1a5629eee9117c2ffbd495101b55b47a10c
shinyu0430 <0430shinyu@gmail.com>
2020-06-07 00:39:19 +0800

    登入改必填

--- login.php
+++ login.php
@@ -62,22 +62,21 @@
                 <div class="single-blog-area clearfix mb-100">
                     <!-- Blog Content -->
                     <div class="single-blog-content">
-                        <h4 style="font-weight: bold; text-align: center;">登入</h4><br />
+                        <h4 style="font-weight: bold; text-align: center;">登入</h4><br>
                         
                         <hr size="10px" style="align:center" width="100%">
-						  
+                       
                         <p style="padding-top:40px">
 							<form action="login_process.php" method="post">
-                            帳號&emsp;&emsp;&nbsp;&emsp;<input type="email" name="account" size="18" class="text animated lightSpeedIn" placeholder="email" style="animation-duration: 1.2s"/><br /><br /> 密碼&emsp;&emsp;&nbsp;&emsp;
-                            <input type="password" name="password" size="18" class="text animated lightSpeedIn" placeholder="password" style="animation-duration: 1.5s"/><br /><br /><br><?php $msg = isset($_GET["msg"])? $_GET["msg"]: "";echo $msg; ?>
+                            帳號&emsp;&emsp;&nbsp;&emsp;<input type="email" name="account" size="18" class="text animated lightSpeedIn" placeholder="email" style="animation-duration: 1.2s" required><br /><br /> 密碼&emsp;&emsp;&nbsp;&emsp;
+                            <input type="password" name="password" size="18" class="text animated lightSpeedIn" placeholder="password" required style="animation-duration: 1.5s"><br><br><br><?php $msg = isset($_GET["msg"])? $_GET["msg"]: "";echo $msg; ?>
                             <a href="registration.html" class="post-tag animated fadeInRight">尚未註冊?</a>
                             <a href="forget.php" class="post-tag animated fadeInLeft">忘記密碼?</a>
                             <br>
-                            <form action="index.php">
+                            
                                 <button type="submit" class="btn btn-outline-dark ">登入</button>
                             </form>
-
-                        </p>
+                    
                     </div>
                 </div>
             </div>
