23d4ea06b3a646411a4457c91e143339ae9e6448
shinyu0430 <0430shinyu@gmail.com>
2020-06-07 00:18:31 +0800

    刪除多得檔案 跟小洞路徑

--- searching.php
+++ searching.php
@@ -40,7 +40,7 @@
         ?>
               <div class="list_item">
           <div class="item_img">
-            <a href="index.php?pro_name=<?php echo $row['pro_name']?>"><img src="<?php echo $row['pro_pic']?>"></a>
+            <a href="searchmap.php?pro_name=<?php echo $row['pro_name']?>"><img src="<?php echo $row['pro_pic']?>"></a>
           </div>
           <div class="item_name text-center">
             <p><?php echo $row['pro_name']?></p>
