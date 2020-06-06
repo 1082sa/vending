68c6372fb1e59f72cad317a44c588ab6eb35572a
Andy <tty09183@gmail.com>
2020-06-06 23:29:51 +0800

    統一button樣式

--- login.php
+++ login.php
@@ -68,13 +68,13 @@
 						  
                         <p style="padding-top:40px">
 							<form action="login_process.php" method="post">
-                            帳號&emsp;&emsp;&nbsp;&emsp;<input type="email" name="account" size="18" class="text animated lightSpeedIn" placeholder="email" style="animation-duration: 2s"/><br /><br /> 密碼&emsp;&emsp;&nbsp;&emsp;
-                            <input type="password" name="password" size="18" class="text animated lightSpeedIn" placeholder="password" style="animation-duration: 2.5s"/><br /><br /><br><?php $msg = isset($_GET["msg"])? $_GET["msg"]: "";echo $msg; ?>
-                            <a href="registration.html" class="post-tag animated bounceInDown">尚未註冊?</a>
-                            <a href="forget.php" class="post-tag">忘記密碼?</a>
+                            帳號&emsp;&emsp;&nbsp;&emsp;<input type="email" name="account" size="18" class="text animated lightSpeedIn" placeholder="email" style="animation-duration: 1.2s"/><br /><br /> 密碼&emsp;&emsp;&nbsp;&emsp;
+                            <input type="password" name="password" size="18" class="text animated lightSpeedIn" placeholder="password" style="animation-duration: 1.5s"/><br /><br /><br><?php $msg = isset($_GET["msg"])? $_GET["msg"]: "";echo $msg; ?>
+                            <a href="registration.html" class="post-tag animated fadeInRight">尚未註冊?</a>
+                            <a href="forget.php" class="post-tag animated fadeInLeft">忘記密碼?</a>
                             <br>
                             <form action="index.php">
-                                <button type="submit" class="btn original-btn">登入</button>
+                                <button type="submit" class="btn btn-outline-dark ">登入</button>
                             </form>
 
                         </p>
