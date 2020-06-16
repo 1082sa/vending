5d336a3badf1aa2d6e6c2c72a81ccfce67d7f5ef
shinyu0430 <0430shinyu@gmail.com>
2020-06-16 16:57:44 +0800

    error_record account值

--- error_record_process.php
+++ error_record_process.php
@@ -1,7 +1,7 @@
 <?php
+$account=$_SESSION["account"];
 
-
-  $sql="SELECT ROW_NUMBER() OVER(ORDER BY Date(error_date)DESC) AS ROWID ,ven_num,error_words, Date(error_date)as error_date,finish FROM `error`";
+  $sql="SELECT ROW_NUMBER() OVER(ORDER BY Date(error_date)DESC) AS ROWID ,ven_num,error_words, Date(error_date)as error_date,finish FROM `error` where `account`='$account'";
   $sth = $conn->query($sql);
 
 
