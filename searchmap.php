87f6986be1b2413f8574acf60720b0eac5dc22d5
Andy0223 <62734347+Andy0223@users.noreply.github.com>
2020-06-18 20:32:36 +0800

    調整searchmap 的大小

--- searchmap.php
+++ searchmap.php
@@ -47,7 +47,7 @@
         html,
         body,
         #map {
-            height: 100%;
+            height: 96.3%;
             margin: 0;
             padding: 20;
         }
@@ -132,19 +132,7 @@
         </div>
     </div>
 
-    <footer>
-        <!-- 到時候要註解掉 -->
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;
-        <script>
-            document.write(new Date().getFullYear());
-        </script>
-        All rights reserved | This template is made with
-        <i class="fa fa-heart-o" aria-hidden="true"></i> by
-        <a href="https://colorlib.com" target="_blank">Colorlib</a>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-    </footer>
-    <!-- ##### Footer Area End ##### -->
+    
 
     <!-- jQuery (Necessary for All JavaScript Plugins) -->
     <script src="js/jquery/jquery-2.2.4.min.js"></script>
