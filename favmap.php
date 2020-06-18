50eea4619fdecef7c55a2d4c0d18759fb61f2754
Andy0223 <62734347+Andy0223@users.noreply.github.com>
2020-06-18 19:41:01 +0800

    調整 我的最愛的地圖大小

--- favmap.php
+++ favmap.php
@@ -47,7 +47,7 @@
         html,
         body,
         #map {
-            height: 100%;
+            height: 96.3%;
             margin: 0;
             padding: 20;
         }
@@ -130,18 +130,7 @@
             </div>
         </div>
     </div>
-    <footer>
-        <!-- 到時候要註解掉 -->
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;
-        <script>
-            document.write(new Date().getFullYear());
-        </script>
-        All rights reserved | This template is made with
-        <i class="fa fa-heart-o" aria-hidden="true"></i> by
-        <a href="https://colorlib.com" target="_blank">Colorlib</a>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-    </footer>
+    
     <!-- ##### Footer Area End ##### -->
 
     <!-- jQuery (Necessary for All JavaScript Plugins) -->
